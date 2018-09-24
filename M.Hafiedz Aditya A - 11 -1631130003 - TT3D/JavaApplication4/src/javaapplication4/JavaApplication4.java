9ee54b9878ebe36e382e3af92251646d041a6a19
HafiedzA <43539696+HafiedzA@users.noreply.github.com>
2018-09-24 18:57:47 +0700

    Add files via upload

---
+++ M.Hafiedz Aditya A - 11 -1631130003 - TT3D/JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author ACER
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 24;
+    Boolean cantik = true;
+    String nama = "hafiedz";
+  
+     if ( umur > 23 && cantik ==true){
+         System.out.println("saya mau menikah");
+         
+     }else if ( umur >= 23  ){
+         System.out.println("saya pikir pikir dulu");
+   
+         
+     
+     } else  {
+         System.out.println("maaf saya tidak mau");
+                 }
+         
+    
+    
+     }
+     
+         
+         
+      
+    }
+        // TODO code application logic here
+    
+    
+
